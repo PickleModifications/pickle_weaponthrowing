775153413bab4ed3d0abe6357ebd6e3217448895
Pickle <111543470+PickleModifications@users.noreply.github.com>
2023-06-03 03:57:34 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ lua54 'yes'
 game 'gta5'
 
 name         'pickle_weaponthrowing'
-version      '1.1.0'
+version      '1.1.1'
 description  'A multi-framework weapon throwing script.'
 author       'Pickle Mods'
 
